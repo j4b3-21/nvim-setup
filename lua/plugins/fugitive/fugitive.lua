3a3e0468ec3825af740cd16854bee2bc7f658c4e
Bhuvan_M_Acharya <bhuvanm200623@gmail.com>
2026-04-07 09:45:19 +0530

    update fugitive

--- lua/plugins/fugitive/fugitive.lua
+++ lua/plugins/fugitive/fugitive.lua
@@ -1,1 +1,10 @@
-return { "tpope/vim-fugitive", cmd = { "Git", "G" } }
+return {
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status" })
+		vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
+		vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git push" })
+		vim.keymap.set("n", "<leader>gl", ":Git pull<CR>", { desc = "Git pull" })
+		vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git blame" })
+	end,
+}
